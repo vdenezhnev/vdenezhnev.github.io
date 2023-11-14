320d6d76d5201acae201f1843e591ab9a2b2e6e3
vdenejhnev <97020290+vdenejhnev@users.noreply.github.com>
2023-11-14 18:47:09 +0000

    add formuls | add chart | add mobile version

---
+++ tax-theorem/chart.js
@@ -0,0 +1,30 @@
+const ctx = document.getElementById('mychart')
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        datasets: [{
+            label: 'studens',
+            data: [60000, 90000, 130000, 160000, 200000, 240000, 270000, 300000, 340000, 390000],
+            backgroundColor: [
+                "#F8F8F8"
+            ],
+
+            borderColor: [
+                '#800080',
+
+            ],
+            borderWidth: 4,
+            pointStyle: false
+        }],
+    },
+    options: {
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+        },
+    }
+});
